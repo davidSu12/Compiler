398f86798e260d4687e76867be8aca29a79845e8
davidSu12 <david.suarez0501@gmail.com>
2025-07-27 00:35:29 +0200

    [iterative-parser] Added stack for iterative parser

--- src/parser/LL/iterative/stack_production.c
+++ src/parser/LL/iterative/stack_production.c
@@ -1,3 +1,65 @@
-//
-// Created by sam_s on 26/07/2025.
-//
+#include "stack_production.h"
+
+
+
+static stackProduction stackProd = NULL;
+
+
+static bool pushLabel(enum labelTok label){
+    nodeProduction * temp = malloc(sizeof(struct nodeProduction));
+    if(!temp){
+        fprintf(stderr, "An error has ocurred while"
+                        "allocating memory for pushLabel\n");
+        return false;
+    }
+    temp -> item = label;
+    temp -> next = stackProd;
+    stackProd = temp;
+    return true;
+}
+
+bool pushProduction(production *el){
+    for(int i = el ->longitud_body; i >= 0; i--){
+        enum labelTok temp = el -> body[i];
+        if(!pushLabel(temp)){
+            /*
+             * i tiene cuantos elementos hemos introducido,
+             * de manera que tenemos que eliminar varios elementos
+             */
+            /*
+             * i = 2 el -> longitud_body = 3
+             */
+            int j = (el -> longitud_body) - i;
+            while(j != 0){
+                popLabel();
+                j--;
+            }
+            return false;
+        }
+    }
+    return true;
+}
+
+enum labelTok peekProduction(){
+    if(!isEmptyStack()){
+        fprintf(stderr, "Stack of labels is empty\n");
+        return -1;
+    }
+    return stackProd -> item;
+}
+
+bool isEmptyStack(){
+    return stackProd == NULL;
+}
+
+void popLabel(){
+    nodeProduction *temp;
+    if(isEmptyStack()){
+        return;
+    }
+    temp = stackProd;
+    stackProd = stackProd -> next;
+    free(temp);
+
+
+}
--- src/parser/LL/iterative/stack_production.h
+++ src/parser/LL/iterative/stack_production.h
@@ -4,31 +4,32 @@
 #include "parser.h"
 
 typedef struct nodeProduction{
-    production item;
-    struct production *next;
+    enum labelTok item;
+    struct nodeProduction *next;
 }nodeProduction;
 
+typedef struct nodeProduction * stackProduction;
 
 /**
  * @param el element to introduce in the stack
  * @return true if everything goes right false otherwise
  */
-bool push(production el);
+bool pushProduction(production *el);
 
 /**
  * @return element that is in the top of the stack
  */
-production peek();
+enum labelTok peekLabel();
 
 /**
  * @return true if the stack is empty, false otherwise
  */
-bool isEmptyList();
+bool isEmptyStack();
 
 /**
  * pops the element on the top of the stack.
  * If everything goes wrong, then a message is emited
  */
-void pop();
+void popLabel();
 
 #endif
