b0800c71116145e1620e83cc627a4c810d9aa912
davidSu12 <david.suarez0501@gmail.com>
2025-07-26 02:49:40 +0200

    [parser-recursive] Added factor

--- src/parser/LL/predictive/parser.c
+++ src/parser/LL/predictive/parser.c
@@ -21,14 +21,24 @@ void exprP(){
 }
 void term(){
 
+
 }
 void termP(){
 
 }
 void factor(){
 
+    lookahead = getNextToken();
+    if(lookahead -> label == NUM){
+        lookahead = getNextToken();
+    }else{
+        match(LEFTPAR);
+        expr();
+        match(RIGHTPAR);
+    }
 }
 void match(enum labelTok tok){
+    lookahead = getNextToken();
     if(tok != lookahead -> label){
         fprintf(stderr, "Syntax error. Aborting compilation\n");
         exit(EXIT_FAILURE);
