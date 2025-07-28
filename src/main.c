126cc6399d9fdd0ee73959eb09b6d6ac0926fd7a
davidSu12 <david.suarez0501@gmail.com>
2025-07-29 01:39:21 +0200

    [iterative-parser] Added proper test files for parseTable

--- src/main.c
+++ src/main.c
@@ -24,98 +24,18 @@
 #ifdef TEST_FOLLOW
 #endif //TEST_FOLLOW
 
-void testParseTable1(void); //EXPR
-void testParseTable2(void); //TERM
-void testParseTable3(void); //EXPRP
-void testParseTable4(void); //TERMP
-void testParseTable5(void); //FACTOR
+
+#ifdef TEST_PARSE_TABLE
+#include "parser/LL/iterative/testParseTable/testParseTable.h"
+#endif //TEST_PARSE_TABLE
+
 
 int main(int argc, char ** argv) {
 
 
-    testParseTable1();
-    testParseTable2();
-    testParseTable3();
-    testParseTable4();
-    testParseTable5();
-    /*
-    setLabel t = first(EXPRP);
-    printSetLabel(t);
-*/
+
 
     /*Nos falta considerar la cadena vacia*/
 
     return 0;
-}
-
-
-void testParseTable1(void){
-    initParseTable();
-    assert(parseTable[VARIABLE_INDEX(EXPR)][TERMINAL_INDEX(LEFTPAR)] == &listProduction[0]);
-    assert(parseTable[VARIABLE_INDEX(EXPR)][TERMINAL_INDEX(RIGHTPAR)] == NULL);
-    assert(parseTable[VARIABLE_INDEX(EXPR)][TERMINAL_INDEX(NUM)] == &listProduction[0]);
-    assert(parseTable[VARIABLE_INDEX(EXPR)][TERMINAL_INDEX(PLUS)] == NULL);
-    assert(parseTable[VARIABLE_INDEX(EXPR)][TERMINAL_INDEX(MINUS)] == NULL);
-    assert(parseTable[VARIABLE_INDEX(EXPR)][TERMINAL_INDEX(DOT)] == NULL);
-}
-
-
-/*
-production listProduction[] = {
-        {EXPR, (enum labelTok[]){TERM, EXPRP}, 2},
-        {EXPRP, (enum labelTok[]){PLUS, TERM, EXPRP}, 3},
-        {EXPRP, (enum labelTok[]){MINUS, TERM, EXPRP}, 3},
-        {EXPRP, NULL, 0},
-        {TERM, (enum labelTok[]){FACTOR, TERMP}, 2},
-        {TERMP, (enum labelTok[]){DOT, FACTOR, TERMP}, 3},
-        {TERMP, (enum labelTok[]){DIV, FACTOR, TERMP}, 3},
-        {TERMP, NULL, 0},
-        {FACTOR, (enum labelTok[]){NUM},1},
-        {FACTOR, (enum labelTok[]){LEFTPAR,EXPR,RIGHTPAR},3},
-        {EMPTY, NULL, 0} //final de production
-};
- */
-
-void testParseTable2(void){
-    initParseTable();
-    assert(parseTable[VARIABLE_INDEX(TERM)][TERMINAL_INDEX(LEFTPAR)] == &listProduction[4]);
-    assert(parseTable[VARIABLE_INDEX(TERM)][TERMINAL_INDEX(RIGHTPAR)] == NULL);
-    assert(parseTable[VARIABLE_INDEX(TERM)][TERMINAL_INDEX(NUM)] == &listProduction[4]);
-    assert(parseTable[VARIABLE_INDEX(TERM)][TERMINAL_INDEX(PLUS)] == NULL);
-    assert(parseTable[VARIABLE_INDEX(TERM)][TERMINAL_INDEX(MINUS)] == NULL);
-    assert(parseTable[VARIABLE_INDEX(TERM)][TERMINAL_INDEX(DOT)] == NULL);
-    assert(parseTable[VARIABLE_INDEX(TERM)][TERMINAL_INDEX(DIV)] == NULL);
-}
-void testParseTable3(void){
-
-    initParseTable();
-    assert(parseTable[VARIABLE_INDEX(EXPRP)][TERMINAL_INDEX(LEFTPAR)] == NULL);
-    assert(parseTable[VARIABLE_INDEX(EXPRP)][TERMINAL_INDEX(RIGHTPAR)] == &listProduction[3]);
-    assert(parseTable[VARIABLE_INDEX(EXPRP)][TERMINAL_INDEX(NUM)] == NULL);
-    assert(parseTable[VARIABLE_INDEX(EXPRP)][TERMINAL_INDEX(PLUS)] == &listProduction[1]);
-    assert(parseTable[VARIABLE_INDEX(EXPRP)][TERMINAL_INDEX(MINUS)] == &listProduction[2]);
-    assert(parseTable[VARIABLE_INDEX(EXPRP)][TERMINAL_INDEX(DOT)] == NULL);
-    assert(parseTable[VARIABLE_INDEX(EXPRP)][TERMINAL_INDEX(DIV)] == NULL);
-
-}
-
-void testParseTable4(void){
-    initParseTable();
-    assert(parseTable[VARIABLE_INDEX(TERMP)][TERMINAL_INDEX(LEFTPAR)] == NULL);
-    assert(parseTable[VARIABLE_INDEX(TERMP)][TERMINAL_INDEX(RIGHTPAR)] == &listProduction[7]);
-    assert(parseTable[VARIABLE_INDEX(TERMP)][TERMINAL_INDEX(NUM)] == NULL);
-    assert(parseTable[VARIABLE_INDEX(TERMP)][TERMINAL_INDEX(PLUS)] == &listProduction[7]);
-    assert(parseTable[VARIABLE_INDEX(TERMP)][TERMINAL_INDEX(MINUS)] == &listProduction[7]);
-    assert(parseTable[VARIABLE_INDEX(TERMP)][TERMINAL_INDEX(DOT)] == &listProduction[5]);
-    assert(parseTable[VARIABLE_INDEX(TERMP)][TERMINAL_INDEX(DIV)] == &listProduction[6]);
-}
-void testParseTable5(void){
-    initParseTable();
-    assert(parseTable[VARIABLE_INDEX(FACTOR)][TERMINAL_INDEX(LEFTPAR)] == &listProduction[9]);
-    assert(parseTable[VARIABLE_INDEX(FACTOR)][TERMINAL_INDEX(RIGHTPAR)] == NULL);
-    assert(parseTable[VARIABLE_INDEX(FACTOR)][TERMINAL_INDEX(NUM)] == &listProduction[8]);
-    assert(parseTable[VARIABLE_INDEX(FACTOR)][TERMINAL_INDEX(PLUS)] == NULL);
-    assert(parseTable[VARIABLE_INDEX(FACTOR)][TERMINAL_INDEX(MINUS)] == NULL);
-    assert(parseTable[VARIABLE_INDEX(FACTOR)][TERMINAL_INDEX(DOT)] == NULL);
-    assert(parseTable[VARIABLE_INDEX(FACTOR)][TERMINAL_INDEX(DIV)] == NULL);
 }
