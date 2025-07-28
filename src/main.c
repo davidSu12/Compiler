5878d75ceb4eae13d3728e974f786db51de89664
davidSu12 <david.suarez0501@gmail.com>
2025-07-28 22:02:26 +0200

    [iterative-parser] Test parse table

--- src/main.c
+++ src/main.c
@@ -24,10 +24,52 @@
 #ifdef TEST_FOLLOW
 #endif //TEST_FOLLOW
 
+void testParseTable1(void); //EXPR
+void testParseTable2(void); //TERM
+void testParseTable3(void); //EXPRP
+void testParseTable4(void); //TERMP
+void testParseTable5(void); //FACTOR
+
 int main(int argc, char ** argv){
 
+    testParseTable2();
+    return 0;
+}
+
+
+void testParseTable1(void){
     initParseTable();
     assert(parseTable[VARIABLE_INDEX(EXPR)][TERMINAL_INDEX(LEFTPAR)] == &listProduction[0]);
+    assert(parseTable[VARIABLE_INDEX(EXPR)][TERMINAL_INDEX(RIGHTPAR)] == NULL);
+    assert(parseTable[VARIABLE_INDEX(EXPR)][TERMINAL_INDEX(NUM)] == &listProduction[0]);
+    assert(parseTable[VARIABLE_INDEX(EXPR)][TERMINAL_INDEX(PLUS)] == NULL);
+    assert(parseTable[VARIABLE_INDEX(EXPR)][TERMINAL_INDEX(MINUS)] == NULL);
+    assert(parseTable[VARIABLE_INDEX(EXPR)][TERMINAL_INDEX(DOT)] == NULL);
+}
+/*
+production listProduction[] = {
+        {EXPR, (enum labelTok[]){TERM, EXPRP}, 2},
+        {EXPRP, (enum labelTok[]){PLUS, TERM, EXPRP}, 3},
+        {EXPRP, (enum labelTok[]){MINUS, TERM, EXPRP}, 3},
+        {EXPRP, NULL, 0},
+        {TERM, (enum labelTok[]){FACTOR, TERMP}, 2},
+        {TERMP, (enum labelTok[]){DOT, FACTOR, TERMP}, 3},
+        {TERMP, (enum labelTok[]){DIV, FACTOR, TERMP}, 3},
+        {TERMP, NULL, 0},
+        {FACTOR, (enum labelTok[]){NUM},1},
+        {FACTOR, (enum labelTok[]){LEFTPAR,EXPR,RIGHTPAR},3},
+        {EMPTY, NULL, 0} //final de production
+};
+ */
+void testParseTable2(void){
 
-    return 0;
+    initParseTable();
+    assert(parseTable[VARIABLE_INDEX(EXPRP)][TERMINAL_INDEX(LEFTPAR)] == NULL);
+    assert(parseTable[VARIABLE_INDEX(EXPRP)][TERMINAL_INDEX(RIGHTPAR)] == NULL);
+    assert(parseTable[VARIABLE_INDEX(EXPRP)][TERMINAL_INDEX(NUM)] == NULL);
+    assert(parseTable[VARIABLE_INDEX(EXPRP)][TERMINAL_INDEX(PLUS)] == NULL);
+    assert(parseTable[VARIABLE_INDEX(EXPRP)][TERMINAL_INDEX(MINUS)] == NULL);
+    assert(parseTable[VARIABLE_INDEX(EXPRP)][TERMINAL_INDEX(DOT)] == NULL);
+    assert(parseTable[VARIABLE_INDEX(EXPRP)][TERMINAL_INDEX(DIV)] == NULL);
+
 }
