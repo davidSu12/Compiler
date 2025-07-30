6d9f6cd6d4ff676cc6569295836f48abe7925474
davidSu12 <david.suarez0501@gmail.com>
2025-07-30 23:47:49 +0200

    [SLR] Added closure function. Pending to test it.

--- src/parser/LR/SLR/parser.c
+++ src/parser/LR/SLR/parser.c
@@ -2,7 +2,34 @@
 
 
 setItem * closure(setItem *st1){
-    return NULL;
+    setItem J;
+    createEmptySetItem(&J);
+    unionSetItems(&J, st1);
+
+    bool added;
+    do{
+        added = false;
+        for(int i = 0; i < NUM_PRODUCCIONES; i++){
+            uint32_t temp = J[i]; //guardo el set temporalmente.
+            while(temp != 0){
+                int k_index = 0;
+                int j = 0;
+                j = temp & 1U; // el indice j se encuentra si o no en el conjunto
+                if(j == 1){
+                    item temp_item = (struct item){i,k_index};
+                    if(!itemInSet(J, temp_item)){
+                        addItem(&J,temp_item);
+                        added = true;
+                    }
+                }
+                temp = temp >> 1;
+                k_index++;
+                j = 0;
+
+            }
+        }
+    }while(!added);
+    return &J;
 }
 setItem * gotoFunction(setItem *st1, enum labelTok token){
     return NULL;
